2a3cab1ed17fb7614aa35dd9000afd30eb980ef8
Brayan Valdebenito <brayan.valdebenito@gmail.com>
2021-09-17 19:09:09 -0300

    asd

--- nasa.rb
+++ nasa.rb
@@ -1,9 +1,9 @@
-·#desarrollado por brayan valdebenito
+#desarrollado por brayan valdebenito
 require 'uri'
 require 'net/https'
 require 'openssl'
 require 'json'
-require_relative 'helpers'
+require_relative 'htmlBuilder'
 
 def request
     url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=XDYqviunvXprkLDY44hNeWnLBA1gXgvRgpqp5odz")
@@ -22,11 +22,11 @@ fotos = metodo['photos']
 
 File.open('build_web_page.html','w') do |f|
     f.puts head
-    f.puts photos_count = fotos.size
+    photos_count = fotos.size
     f.puts "<p> Cantidad de fotos :#{photos_count}</p> "
     f.puts "<ul>"
     fotos.each do|foto|
-    f.puts "<li><img src = '#{foto['img_src']}'></li>"
+    f.puts "<li><img src = '#{foto['img_src']}' width='400' height= '400' class='img-fluid rounded'></li>"
     f.puts "</ul>"
     end
     f.puts footer
